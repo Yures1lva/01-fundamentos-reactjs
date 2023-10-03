1d1bd2293e4fc9d0fbd6f61dab5d903500ca52e8
yuro <yure.siilva@gmail.com>
2023-10-03 10:59:57 -0300

    bibli de icones e estilizando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { Post } from './Post'
-import { Header } from './components/header'
+import styles from "./App.module.css"
+import { Header } from './components/Header'
 
 import "./global.css"
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
 
   return (
     <>
-    <Header/>
+      <Header/>
 
-      <h1>hello world</h1>
-      <Post 
-        author="yuro" 
-        content="text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-      />
-      <Post/>
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="yuro"
+            content="k a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem"
+          />
 
+<Post
+            author="yuro"
+            content="k a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem"
+          />
+        </main>
+      </div>
+        
       
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg';
 
 export function Header(){
     return(
         <>
             <header className={styles.header}>
-            <strong  >Ignite Feed</strong>
+                <img src={igniteLogo} alt="Logo do ignite" />
+               <strong  >Ignite Feed</strong>
             </header>
         </>
     )
