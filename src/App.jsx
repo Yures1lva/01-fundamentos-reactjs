2e2e65e13fed373f38bedbe4bbc8fff9f6f42d6d
Yure <yure.siilva9@gmail.com>
2023-10-02 21:37:25 -0300

    começando com Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from './Post'
+import { Header } from './components/header'
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles.css"
+
+export function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Header/>
+
+      <h1>hello world</h1>
+      <Post 
+        author="yuro" 
+        content="text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      />
+      <Post/>
+
+      
     </>
   )
 }
 
-export default App
+
---
+++ src/Post.jsx
@@ -0,0 +1,15 @@
+
+
+export function Post(props){
+    
+    
+
+    return (
+        <>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+
+        </>
+    )
+}
+
