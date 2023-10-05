8af73ce36fb13de9860e974ba3fbcf56a3e66fe2
yuro <yure.siilva@gmail.com>
2023-10-05 15:47:57 -0300

    validando form e melhorando submit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,31 +8,49 @@ import { useState } from "react"
 
 
 export function Post({author, publishedAt, content}){
+    //constates do dateTime
     const publishedDatFormated = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {locale: ptBR})
     const publishedDateToNow = formatDistanceToNow(publishedAt, {locale: ptBR, addSuffix: true},)
 
+    //variaveis para armazenar estados do comentário
+    //novo estado, estadoatual
     const [newCommentText, setNewCommentText] = useState('')
 
+    //setando lista de comentários 
     const [comments, setComments] = useState([
         "Post dahora em"
     ])
 
+    // função de controle do forms
      function handleCreateNewComment(){
+        //nãoa atualizar a pagina
         event.preventDefault()
+
+        //setando nova lista, com os existentes mais o novo
         setComments([...comments, newCommentText])
+        //setando estado pra null novamente
         setNewCommentText('')
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
     }
 
+    //função para deletar um item da lista especificada
     function deleteComment(commmentToDelete){
         const commentsWithouDeletedOne = comments.filter(comment => {
             return comment !== commmentToDelete
         })
         setComments(commentsWithouDeletedOne)
     }
+
+    function handleNewCommentValid(){
+        event.target.setCustomValidity('Este campo é obrigatório')
+
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -45,7 +63,10 @@ export function Post({author, publishedAt, content}){
                         <span>{author.role}</span>
                     </div>
                 </div>
-                <time title={publishedDatFormated} dateTime={publishedAt.toISOString()} >
+                <time 
+                    title={publishedDatFormated} 
+                    dateTime={publishedAt.toISOString()} 
+                >
                     {publishedDateToNow}
                 </time>
             </header>
@@ -55,22 +76,33 @@ export function Post({author, publishedAt, content}){
                     if(line.type === 'paragraph'){
                         return <p key={line.content} >{line.content}</p>;
                     } else if(line.type === 'link'){
-                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a
+                         href="#">{line.content}</a></p>
                     }
                 })}
 
             </div>
-            <form  onSubmit={handleCreateNewComment} className={styles.commentForm}>
+            <form  
+                onSubmit={handleCreateNewComment} 
+                className={styles.commentForm}
+            >
                 <strong>Deixe seu feedback</strong>
 
                 <textarea name="comment"
-                 placeholder="Deixe um comentário"
-                 onChange={handleNewCommentChange}
-                 value={newCommentText}
+                    placeholder="Deixe um comentário"
+                    onChange={handleNewCommentChange}
+                    value={newCommentText}
+                    required
+                    onInvalid={handleNewCommentValid}
                  ></textarea>
 
                 <footer className={styles.commentFooter}>
-                    <button className={styles.buttonForm} type="submit">Publicar</button>
+                    <button 
+                        className={styles.buttonForm} 
+                        disabled={isNewCommentEmpty}
+                        type="submit"
+                    >Publicar
+                    </button>
                 </footer>
 
                 <div className={styles.commentList}>
